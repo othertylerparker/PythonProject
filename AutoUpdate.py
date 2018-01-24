ea68d5b8dec2ae21ab619dacdbd140c2bf8e9f41
othertylerparker <35426290+othertylerparker@users.noreply.github.com>
2018-01-24 20:38:19 +0000

    Update AutoUpdate.py

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -12,30 +12,36 @@ def autoUpdate():
     file_name = verFile.split('/')[-1]
     file = open(file_name, 'wb')
     f = open("AutoUpdate.py", 'r')
-    verCheck = filecmp.cmp('AutoUpdate.py', file)
-    if verCheck == True:
-        print "Files match"
-    else:
-        print "Files do not match. Updating."
-        u = urllib.urlopen(verFile)
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading: %s Bytes: %s" % (file_name, file_size)
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            file.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8) * (len(status) + 1)
-            print status,
-
-        file.close()
-        f.close()
+    with open(file, 'r') as file1:
+        with open(f, 'r') as file2:
+            same = set(file1).intersection(file2)
+    same.discard('\n')
+    with open('testForSame.txt', 'w') as file_out:
+        for line in same:
+            output = file_out.write(line)
+            if output == "":
+                print "Files match"
+            else:
+                print "Files do not match. Updating."
+                u = urllib.urlopen(verFile)
+                meta = u.info()
+                file_size = int(meta.getheaders("Content-Length")[0])
+                print "Downloading: %s Bytes: %s" % (file_name, file_size)
+                file_size_dl = 0
+                block_sz = 8192
+                while True:
+                    buffer = u.read(block_sz)
+                    if not buffer:
+                        break
+        
+                    file_size_dl += len(buffer)
+                    file.write(buffer)
+                    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                    status = status + chr(8) * (len(status) + 1)
+                    print status,
+        
+                file.close()
+                f.close()
 while True:
     autoUpdate()
     with open('userpass') as f:
